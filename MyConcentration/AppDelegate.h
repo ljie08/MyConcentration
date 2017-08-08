b28a4043d199f6a75e96edd63a65405c0227b1e5
ljie <ljie@ljie.local>
2017-08-08 16:08:42 +0800

    Initial Commit

---
+++ MyConcentration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyConcentration
+//
+//  Created by ljie on 2017/8/8.
+//  Copyright © 2017年 AppleFish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
